9b66df98e691ab490ec6216145ab3f0ffa83726a
Hussain Amet <hussainamet@gmail.com>
2024-02-16 00:02:03 +0530

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,26 @@
-import { defineConfig, loadEnv  } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createServer } from 'http';
 
-const env = loadEnv(
-    'all',
-    process.cwd()
-);
+const env = loadEnv('all', process.cwd());
+const productionUrl = env.VITE_PRODUCTION_URL || "http://localhost:3001";
 
-let productionUrl = env.VITE_PRODUCTION_URL;
+// Function to set allowed methods
+function allowMethods(req, res, next) {
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+}
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/association-funds" : productionUrl || "http://localhost:3001",
+      "/association-funds": {
+        target: productionUrl,
+        middleware: allowMethods
+      }
     },
   },
   plugins: [
     react(),
   ],
-})
+});
