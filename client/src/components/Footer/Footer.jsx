fdcd3513a967ab71774e1c73eaf040b579a0423f
Hussain Amet <hussainamet@gmail.com>
2024-04-28 21:34:25 +0530

    4/28/24 done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='d-flex bg-dark-subtle fs-6 justify-content-center ps-4 gap-sm-5' >
-      <p className='mb-0 me-4'>Hussain Amet</p>
-      <p className='mb-0'>8739975253</p>
+    <div className='bg-dark-subtle fs-6 text-center' >
+      <p className='mb-0'>Developed by: Hussain Amet (8739975253)</p>
     </div>
   )
 }
