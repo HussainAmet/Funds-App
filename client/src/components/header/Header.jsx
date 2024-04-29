8d85bf2b11d2f6282936512ed80907420c4fef20
Hussain Amet <hussainamet@gmail.com>
2024-04-29 22:28:51 +0530

    4/29/24 done

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,6 +13,10 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/memberDetailsSlice';
+import Tooltip from '@mui/material/Tooltip';
+import InfoIcon from '@mui/icons-material/Info';
+import Zoom from '@mui/material/Zoom';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 import './header.css'
 // import Footer from '../Footer/Footer'
 
@@ -23,6 +27,16 @@ export default function Header() {
 
   const data = useSelector((state) => state.member.memberDetails)
   const dispatch = useDispatch();
+
+  const [open, setOpen] = useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
 
   const toggleDrawer = (open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -112,7 +126,29 @@ export default function Header() {
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 Association Funds
               </Typography>
-              {memberData?.auth?.data?.role? <Button color="inherit" onClick={logo}>Logout</Button> : ''}
+              <div className='d-flex'>
+                {!memberData?.auth?.data?.role.includes('host') ?
+                  <ClickAwayListener onClickAway={handleTooltipClose}>
+                      <Tooltip
+                        PopperProps={{
+                            disablePortal: true,
+                        }}
+                        onClose={handleTooltipClose}
+                        TransitionComponent={Zoom}
+                        open={open}
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                        title="Developer: Hussain Amet (8739975253)"
+                      >
+                        <button onClick={handleTooltipOpen} className='border-0 bg-transparent me-3' ><InfoIcon className='text-white' /></button>
+                      </Tooltip>
+                  </ClickAwayListener>
+                :
+                  ''
+                }
+                {memberData?.auth?.data?.role? <Button color="inherit" onClick={logo}>Logout</Button> : ''}
+              </div>
           </Toolbar>
         </AppBar>
       </Box>
