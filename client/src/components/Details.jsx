b765c1af6831ec13202037961ca1ee62c430e930
Hussain Amet <hussainamet@gmail.com>
2024-06-01 20:39:48 +0530

    Update Details.jsx

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -46,7 +46,7 @@ export default function Details() {
       const detailsData = getYear();
       let details = [];
       detailsData?.map((detail) => {
-        if (Number(String(detail.year.split(" ", 1))) === event.target.value) {
+        if (Number(detail.year.split(" ", 1)) === event.target.value) {
           details.push(detail);
         }
       });
