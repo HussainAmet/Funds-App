dff179433b1938ac011cba3504a3deb8257e6aef
Hussain Amet <associationfunds53@gmail.com>
2024-11-22 18:12:10 +0530

    commit13

--- server/server.js
+++ server/server.js
@@ -40,7 +40,9 @@ app.get("/", (req, res) => {
 app.post(`${config.requestBaseUrl}login`, async (req, res) => {
   const number = String(req.body.phone);
   try {
+    console.log("123");
     const userData = await userModel.findOne({ "data.phone": number });
+    console.log("456");
     if (userData) {
       if (userData.data.active === true) {
         if (userData.data.role.includes("host")) {
