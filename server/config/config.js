d258c876f10da1d09b6308fbe58ce0d04fb85c32
Hussain Amet <hussainamet@gmail.com>
2024-12-05 19:15:35 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,8 @@ dotenv.config();
 const config = {
     mongodUri: String(process.env.MONGOD_SERVER_URI),
     port: process.env.PORT,
-    requestBaseUrl: String(process.env.REQUEST_BASE_URL)
+    requestBaseUrl: String(process.env.REQUEST_BASE_URL),
+    productionUrl: String(process.env.PRODUCTION_URL),
 }
 
 export default config;
